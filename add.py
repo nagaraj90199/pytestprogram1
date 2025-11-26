c4fa9e36c5ebec7e7e52efc1ea1f231d87bc45a5
Nagaraj Kuntoji <kn733478@gmail.com>
2025-11-26 10:46:37 +0530

    Refactor add function and update input method

--- add.py
+++ add.py
@@ -1,7 +1,9 @@
-def add(a,b):
+def add(a, b):
     return a + b
 
 if __name__ == "__main__":
-    x=10
-    y=20
-    print("Sum:", add(x,y))
+    # Take user input
+    x = int(input("Enter first number: "))
+    y = int(input("Enter second number: "))
+    
+    print("Sum:", add(x, y))
