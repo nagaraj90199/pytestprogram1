45b880fd25c0b3a7d63323e6518971bdced7345c
Nagaraj Kuntoji <kn733478@gmail.com>
2025-11-26 11:04:30 +0530

    Update add.py

--- add.py
+++ add.py
@@ -2,8 +2,14 @@ def add(a, b):
     return a + b
 
 if __name__ == "__main__":
-    # Take user input
-    x = int(input("Enter first number: "))
-    y = int(input("Enter second number: "))
-    
+    try:
+        # Try to take user input
+        x = int(input("Enter first number: "))
+        y = int(input("Enter second number: "))
+    except Exception:
+        # If input fails, use default values
+        print("Input failed, using default values...")
+        x = 10
+        y = 20
+
     print("Sum:", add(x, y))
